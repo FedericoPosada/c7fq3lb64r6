71d29a9f091b1cc90325290844825f70656bea3f
FedericoPosada2 <fedeposa122@gmail.com>
2020-02-25 18:17:08 -0500

    Commit

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,17 @@
 class ExpensesController < ApplicationController
-  def index
-   @expenses = Expense.order("date DESC")
-  end
+	def index
+		if(!params[:concept].nil? and !params[:category_id].nil?)
+			if(params[:concept].empty? and params[:category_id].empty?)
+				@expenses = Expense.order("date DESC")
+			elsif(!params[:concept].empty? and !params[:category_id].empty?)
+				@expenses = Expense.where("category_id like ?","%#{params[:category_id]}").where("concept like ?","%#{params[:concept]}").order("date DESC")
+			elsif(!params[:concept].empty?)
+				@expenses = Expense.where("concept like ?","%#{params[:concept]}").order("date DESC")
+			else
+				@expenses = Expense.where("category_id like ?","%#{params[:category_id]}").order("date DESC")
+			end
+		else
+			@expenses = Expense.order("date DESC")
+		end
+	end
 end
